e2fff0219d79402635ae665648458551d433a091
Meldin Geljic <meldingeljic@Mac-mini.local>
2019-12-11 05:26:22 +0100

    Navbar component & Router functionality

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Recipes from "./pages/Recipes";
 import SingleRecipe from "./pages/SingleRecipe";
 import Default from "./pages/Default";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <main>
-        {/* navbar */}
+        <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/recipes" exact component={Recipes} />
