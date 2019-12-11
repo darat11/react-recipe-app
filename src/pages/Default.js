1c8f12f437bea76da1ab4f9f9e3d4dea80d7033f
Meldin Geljic <meldingeljic@Mac-mini.local>
2019-12-11 15:59:49 +0100

    Default/error/not-found page component

--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,7 +1,21 @@
 import React, { Component } from "react";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 export default class Default extends Component {
   render() {
-    return <h4>Selam from ErrorPage</h4>;
+    return (
+      <Header title="404" styleClass="default-hero">
+        <h2 className="text-light text-uppercase">
+          You are in the wrong place
+        </h2>
+        <Link
+          to="/"
+          className="text-uppercase btn btn-secondary btn-large mt-3"
+        >
+          Return home
+        </Link>
+      </Header>
+    );
   }
 }
